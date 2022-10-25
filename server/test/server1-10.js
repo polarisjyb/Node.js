32f2378afd0bf91aab70d7f28a186e263dd9b38c
Violet Ultra <evia_a@naver.com>
2022-10-26 01:10:20 +0900

    서버 가동 기본 연습

---
+++ server/test/server1-10.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, {"Content-Type": "text/html; charset=utf-8" });
+  res.write('2022년 10월 26일 오전 0시 54분 <br>');
+  res.end('2022년 10월 26일 오전 1시 8분 응답을 종료합니다.');
+});
+
+app.listen(3000, () => {
+  console.log('3000 포트로 가동된 서버입니다.');
+});
